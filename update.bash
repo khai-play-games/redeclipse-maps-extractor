26bfe05d5afa62d5085b9ca22dd5923ea72ce146
khai96_ <hvksmr1996@gmail.com>
2016-11-15 00:05:46 +0700

    Extract improvement

--- update.bash
+++ update.bash
@@ -1,8 +1,27 @@
 #! /usr/bin/env bash
+
 wdir="$(dirname $0)"
 target="$(./target.bash)"
+
+echo EXTRACTING...
 for archive in $(./list.js)
 do
   echo Uncompressing: $archive → $target
   7za x "$wdir/$archive" "-o$target" -y
+done
+
+echo CORRECTING...
+cd ~/.redeclipse/maps
+for item in $(ls)
+do
+  if [[ -d "$item" ]]
+    then
+      echo Getting stuffs out of $item
+      for subitem in $(ls -A "$item")
+      do
+      	echo '  >' $subitem
+      	mv "$item/$subitem" "../$subitem"
+      done
+      rmdir "$item"
+  fi
 done
