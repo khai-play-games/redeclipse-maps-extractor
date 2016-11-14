2d474c6af1bad2edb1435c0e701c48a89460c7b3
khai96_ <hvksmr1996@gmail.com>
2016-11-15 03:42:34 +0700

    Correctition

--- update.bash
+++ update.bash
@@ -1,27 +1,39 @@
 #! /usr/bin/env bash
 
-wdir="$(dirname $0)"
+wdir="$(pwd)"
 target="$(./target.bash)"
 
+echo CLEANING...
+cd "$target"
+rm -rvf *
+
 echo EXTRACTING...
+cd "$wdir"
 for archive in $(./list.js)
 do
   echo Uncompressing: $archive → $target
   7za x "$wdir/$archive" "-o$target" -y
 done
 
-echo CORRECTING...
-cd ~/.redeclipse/maps
-for item in $(ls)
-do
-  if [[ -d "$item" ]]
+echo FLATTENING...
+function flatten () (
+  if [[ -d "$1" ]]
+  	then
+  	  echo '  d>' $1
+  	  for item in $(ls "$1")
+  	  do
+  	    flatten "$1/$item"
+  	  done
+  	  rmdir "$1"
+  elif [[ -f "$1" ]]
     then
-      echo Getting stuffs out of $item
-      for subitem in $(ls -A "$item")
-      do
-      	echo '  >' $subitem
-      	mv "$item/$subitem" "../$subitem"
-      done
-      rmdir "$item"
+      echo '  f>' $1
+      mv "$1" "$target"
+  fi
+)
+for item in $(ls "$target")
+do
+  if [[ -d "$target/$item" ]]
+    then flatten "$target/$item"
   fi
 done
